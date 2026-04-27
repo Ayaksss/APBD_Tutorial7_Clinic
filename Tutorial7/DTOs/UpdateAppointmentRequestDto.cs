94f76ca875417fe0f02599231e98c0bbfae527b6
Illia Smoliaruk <s32770@pjwstk.edu.pl>
2026-04-27 21:22:32 +0200

    fixed UpdateAppointmentRequestDto.cs

--- Tutorial7/DTOs/UpdateAppointmentRequestDto.cs
+++ Tutorial7/DTOs/UpdateAppointmentRequestDto.cs
@@ -1,11 +1,18 @@
 ﻿namespace Tutorial7.DTOs;
 
+using System.ComponentModel.DataAnnotations;
+
 public class UpdateAppointmentRequestDto
 {
     public int IdPatient { get; set; }
     public int IdDoctor { get; set; }
     public DateTime AppointmentDate { get; set; }
+    
+    [Required]
     public string Status { get; set; } = string.Empty;
+    
+    [Required]
+    [MaxLength(250)]
     public string Reason { get; set; } = string.Empty;
     public string? InternalNotes { get; set; }
 }
